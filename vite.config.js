3bbb9ef84b744338c1665cc7da486a3ffa5887ff
8k-u <8k.kaas.304@gmail.com>
2024-08-20 02:00:13 +0900

    test7

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,14 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 const root = resolve(__dirname, "src");
-const outDir = resolve(__dirname, "dist");
 
 export default defineConfig({
   root,
   build: {
-    outDir: resolve(__dirname, "dist"),
-    assetsDir: './',
-    publicPath: './',
     rollupOptions: {
       input: {
         main: resolve(root, "index.html"),
-        sample: resolve(root, "sample/index.html"),
+        sample: resolve(root, "sample", "index.html"),
         sample2: resolve(__dirname, "src/sample2/index.html"),
       },
       output: {
