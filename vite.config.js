ddf57027eec874c799f00c44647391f7c394006e
8k-u <8k.kaas.304@gmail.com>
2024-08-20 02:02:12 +0900

    test7-2

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,19 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
 
 export default defineConfig({
   root,
   build: {
+    outDir: resolve(__dirname, "dist"),
     rollupOptions: {
       input: {
         main: resolve(root, "index.html"),
         sample: resolve(root, "sample", "index.html"),
         sample2: resolve(__dirname, "src/sample2/index.html"),
       },
-      output: {
-        entryFileNames: '[name].js',
-      },
+
     },
   },
   base: "/test1/",
